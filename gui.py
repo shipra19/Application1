fdb71d78f389e96348f7911c76de39bdc41ddaa2
ShVa <shipra195@gmail.com>
2026-07-11 23:01:53 +0530

    Commit 2

--- gui.py
+++ gui.py
@@ -1,11 +1,26 @@
 import FreeSimpleGUI
-
+import functions
 
 label=FreeSimpleGUI.Text("Type in a to-do item")
-input_box=FreeSimpleGUI.InputText(tooltip="Enter here")
+input_box=FreeSimpleGUI.InputText(tooltip="Enter here", key="i")
 add_button=FreeSimpleGUI.Button("Add")
 
-window = FreeSimpleGUI.Window("My To-Do App", layout=[[label], [input_box], [add_button]])
-window.read()
-print("Hey")
+window = FreeSimpleGUI.Window("My To-Do App",
+                              layout=[
+                                  [label],
+                                  [input_box, add_button]
+                                      ],
+                              font=("Arial", 20))
+while True:
+    event,values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos=functions.get_todos()
+            new_todo=values["i"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case FreeSimpleGUI.WIN_CLOSED:
+            break
 window.close()
